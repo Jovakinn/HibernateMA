b89e7ed6387ac0d08fa6659f8c65682d37d24f58
38098 <max05012004@gmail.com>
2019-08-27 21:35:22 +0200

    Initial commit

---
+++ src/main/java/com/mainacad/dao/HibernateFactory.java
@@ -0,0 +1,20 @@
+package com.mainacad.dao;
+
+import org.hibernate.SessionFactory;
+import org.hibernate.cfg.Configuration;
+
+public class HibernateFactory {
+
+    private static SessionFactory sessionFactory;
+
+    public static SessionFactory getSessionFactory(){
+
+        if (sessionFactory == null){
+
+            Configuration configuration = new Configuration().configure();
+            sessionFactory = configuration.buildSessionFactory();
+        }
+        return sessionFactory;
+    }
+
+}
