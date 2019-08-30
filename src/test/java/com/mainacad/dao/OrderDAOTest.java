eb10ccf4f26709fb446d684a2cfb31dc3d2f4059
38098 <max05012004@gmail.com>
2019-08-30 12:29:35 +0200

    OrderDAO test refactor

--- src/test/java/com/mainacad/dao/OrderDAOTest.java
+++ src/test/java/com/mainacad/dao/OrderDAOTest.java
@@ -3,7 +3,10 @@ package com.mainacad.dao;
 import com.mainacad.model.Cart;
 import com.mainacad.model.Item;
 import com.mainacad.model.Order;
+import com.mainacad.model.User;
 import org.junit.jupiter.api.Test;
+
+import java.util.List;
 
 import static org.junit.jupiter.api.Assertions.assertNotNull;
 import static org.junit.jupiter.api.Assertions.assertNull;
@@ -12,17 +15,26 @@ class OrderDAOTest {
 
     @Test
     void testSaveAndFindOneAndDelete() {
-        Item item = new Item();
-        Cart cart = new Cart();
+        Item item = new Item("12345", "Perforator",1000 );
+        ItemDAO.save(item);
+
+        User user = new User("testLogin", "123456", "Max", "Khodakov");
+        UserDAO.save(user);
+
+        Cart cart = new Cart( 1l, false, user);
+        CartDAO.save(cart);
 
         Order order = new Order(item, 1, cart);
-
         Order savedOrder = OrderDAO.save(order);
+
         assertNotNull(savedOrder);
         assertNotNull(savedOrder.getId());
 
         Order dbOrder = OrderDAO.findOneById(savedOrder.getId());
         assertNotNull(dbOrder);
+
+        List<Order> checkedOrderByCart = OrderDAO.findByCart(cart.getId());
+        assertNotNull(checkedOrderByCart);
 
         OrderDAO.delete(savedOrder);
 
